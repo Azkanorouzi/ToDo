b89692b2b8e6b5847f5d51a8b7cb158548460cfe
Azka Norouzi <a.azka.norouzi@gmail.com>
2023-07-31 16:21:12 +0330

    🌈 Do brain storming

---
+++ src/views/view.js
@@ -0,0 +1,2 @@
+// Presentation logic,
+// View is responsible for presentation logic, the pages we have the things that we want to appear in the dom basically anything that's related to presenting our state to the user and also to show anything it's all part of the view
