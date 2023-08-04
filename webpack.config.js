6153fa0942efd18ed7276aaf7c5ece344db35f09
Azka Norouzi <a.azka.norouzi@gmail.com>
2023-08-04 18:24:55 +0330

    ✍️  Add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,15 @@ module.exports = {
         type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
