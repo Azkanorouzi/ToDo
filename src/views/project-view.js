b0e7d5b56323e2ea24665f0d4e4cb8eb98eb33ca
Azka Norouzi <a.azka.norouzi@gmail.com>
2023-08-10 16:10:12 +0330

    ✅ Create render and clear method

---
+++ src/views/project-view.js
@@ -0,0 +1,4 @@
+import { View } from './view'
+class ProjectView extends View {}
+
+export { ProjectView }
--- src/views/view.js
+++ src/views/view.js
@@ -1,8 +1,18 @@
 class View {
+  // ParentEl typically would be display container
   #parentEl = document.querySelector('.display-container')
-  render() {
-    this.#parentEl.innerHTML = this.#generateHTML(this.#data)
+  /**
+   * Renders the view
+   * @sideEffect this method renders the display in the parent element of whichever object it was called upon and therefore it modifies the DOM
+   * @param {boolean} clear in case we don't want our parent element to be cleared before injecting our generated html we set this parameter to false (default:true)
+   */
+  render(clear = true) {
+    if (clear) this.#clear()
+    this.#parentEl.innerHTML += this.#generateHTML(this.#assets)
+  }
+  #clear() {
+    this.#parentEl.innerHTML = ''
   }
 }
-// We need to export view because we just want to inherit it's properties and methods
+// We need to export view and not an instance because we just want to inherit it's properties and methods
 export { View }
